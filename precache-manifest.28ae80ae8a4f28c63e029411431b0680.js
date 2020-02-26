19f0bc40cfee54b8a65dbd8e86abb3d92b1828f2
Leah Migacz <lmigacz@msudenver.edu>
2020-02-25 23:34:48 -0700

    Updates

---
+++ precache-manifest.28ae80ae8a4f28c63e029411431b0680.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f5940a0d7f5a9da4901ebd523bcb8b",
+    "url": "/react-directory/index.html"
+  },
+  {
+    "revision": "1a881d3572c7fcb23e85",
+    "url": "/react-directory/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "14eabe1cbd68e11b297f",
+    "url": "/react-directory/static/js/2.56217055.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-directory/static/js/2.56217055.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a881d3572c7fcb23e85",
+    "url": "/react-directory/static/js/main.f6aa5184.chunk.js"
+  },
+  {
+    "revision": "26ae59d4706c9adadab0",
+    "url": "/react-directory/static/js/runtime-main.e5ea28e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-directory/static/media/logo.5d5d9eef.svg"
+  }
+]);
