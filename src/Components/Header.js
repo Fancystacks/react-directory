bac77e577b3f63ac633c3a781a31ebe06f50477a
Leah Migacz <lmigacz@msudenver.edu>
2020-02-26 19:30:20 -0700

    Added a million components

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "../styles/Header.css";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <h1>Directory of Valued Employees</h1>
+      </div>
+    )
+  }
+}
